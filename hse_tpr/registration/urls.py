a21a5b9480ee1d9e66d584893b1acfed74eda7fb
max23.ost <max23.ost@gmail.com>
2023-04-07 04:54:43 +0200

    added login page

--- hse_tpr/registration/urls.py
+++ hse_tpr/registration/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('registration/', RegistrationView.as_view(), name='registration'),
-    
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
